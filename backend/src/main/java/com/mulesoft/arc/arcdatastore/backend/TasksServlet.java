254f89fd4bb4599696c4e87d31ea3f5e729bba53
CI Agent <arc@mulesoft.com>
2017-01-23 21:00:58 +0000

    Update: Added task to schedule analytics worker for particular day/week/month

--- backend/src/main/java/com/mulesoft/arc/arcdatastore/backend/TasksServlet.java
+++ backend/src/main/java/com/mulesoft/arc/arcdatastore/backend/TasksServlet.java
@@ -3,9 +3,12 @@ package com.mulesoft.arc.arcdatastore.backend;
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
 import com.google.appengine.api.taskqueue.TaskOptions;
+import com.google.gson.Gson;
+import com.mulesoft.arc.arcdatastore.backend.models.ErrorResponse;
 
 import java.io.IOException;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.logging.Logger;
@@ -26,6 +29,9 @@ public class TasksServlet extends HttpServlet {
         String path = req.getPathInfo();
         if ("/schedule-analyser".equals(path)) {
             addTasks(resp);
+            return;
+        } else if ("/schedule".equals(path)) {
+            addTasksByType(req, resp);
             return;
         }
 
@@ -74,5 +80,72 @@ public class TasksServlet extends HttpServlet {
         }
 
         resp.setStatus(204);
+    }
+
+    private void addTasksByType(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String type = req.getParameter("type");
+        String date = req.getParameter("date");
+        String error = "";
+
+        if (type == null) {
+            error = "The type parameter is required. Set it to one of \"daily\", \"weekly\" or \"monthly\". ";
+        } else if (!("daily".equals(type) || "weekly".equals(type) || "monthly".equals(type))) {
+            error = "The type parameter is invalid (" + type + "). Set it to one of \"daily\", \"weekly\" or \"monthly\". ";
+        }
+        if (date == null) {
+            error = error.concat("The date parameter is required. Set it to the date in format YYYY-mm-dd for \"daily\" and \"weekly\" types and YYYY-mm for \"monthly\" type.");
+        }
+
+        if (!error.equals("")) {
+            reportError(resp, 400, error);
+            return;
+        }
+
+        String dateFormat = "YYY-mm";
+        if (!type.equals("monthly")) {
+            dateFormat = dateFormat.concat("-dd");
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+        sdf.setLenient(false);
+
+        try {
+            sdf.parse(date);
+        } catch (ParseException e) {
+            reportError(resp, 400, "The date parameter has invalid format. Please, use date in format YYYY-mm-dd for \"daily\" and \"weekly\" types and YYYY-mm for \"monthly\" type.");
+            return;
+        }
+
+        String url = "/analytics/analyse-";
+        String taskName;
+        if (type.equals("daily")) {
+            url = url.concat("day");
+            taskName = "daily";
+        } else if (type.equals("weekly")) {
+            url = url.concat("week");
+            taskName = "weekly";
+        } else {
+            url = url.concat("month");
+            taskName = "monthly";
+        }
+        taskName = taskName.concat("-analyser-" + date);
+        Queue q = QueueFactory.getQueue("analytics-analyser");
+        q.add(TaskOptions.Builder.withUrl(url)
+                .param("date", date)
+                .taskName(taskName)
+                .method(TaskOptions.Method.GET));
+        resp.setStatus(204);
+    }
+
+    private void reportError(HttpServletResponse resp, int statusCode, String message) throws IOException {
+        ErrorResponse r = new ErrorResponse();
+        r.code = statusCode;
+        r.message = message;
+
+        Gson gson = new Gson();
+
+        resp.setStatus(statusCode);
+        resp.setContentType("application/json");
+        resp.getWriter().print(gson.toJson(r));
     }
 }
